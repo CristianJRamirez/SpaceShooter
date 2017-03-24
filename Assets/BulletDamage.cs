ac75ae91043a714642e5b881a75924198b8a747d
CristianJRamirez <45858000w@iespoblenou.org>
2017-03-24 15:52:39 +0100

    proyecto unity 3 acabdo

---
+++ Assets/BulletDamage.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDamage : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D coll){
+		//collided with a ship? then destroy it.
+		if (coll.gameObject.tag=="ship")
+			Destroy(coll.gameObject);
+
+		//Destroy Bullet in any case
+		Destroy(gameObject);	
+	}
+}
